43a1fa9de0a0c156a148051e7c0df07f010370d7
nithinpnandanam <nithinpnandanam@gmail.com>
2025-03-19 18:57:27 +0530

    add pagination for table

--- src/api/allUsers.api.ts
+++ src/api/allUsers.api.ts
@@ -1,6 +1,20 @@
 import { endPoints } from "../constants/endpoints";
 import axiosClient from "../services/axios";
 
-export const fetchAllUsers = () => {
-  return axiosClient.get(endPoints.ALL_USERS);
+export const fetchAllUsers = (limitValue?:number,skipValue?:number) => {
+  return axiosClient.get(endPoints.ALL_USERS,{
+    params:{
+      limit:limitValue,
+      skip:skipValue,
+    }
+  });
 };
+
+// export const fetchAllProducts = (sortBy?:string,order?:string) => {
+//   return axiosClient.get(endPoints.PRODUCTS,{
+//     params: {
+//       order: order ??  '',
+//       sortBy: sortBy ?? '',
+//     },
+//   });
+// };
--- src/components/UserList/UserList.tsx
+++ src/components/UserList/UserList.tsx
@@ -1,84 +1,87 @@
-import { FC, useEffect, useState } from "react";
-import { fetchAllUsers } from "../../api/allUsers.api";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+
+import { fetchAllUsers } from "../../api/allUsers.api";
+
+import { FC, useEffect, useState } from "react";
+import "./UserList.css";
 
 const columns: GridColDef<(typeof rows)[number]>[] = [
-    { field: 'id', headerName: 'ID', width: 90 },
-    {
-      field: 'firstName',
-      headerName: 'First name',
-      editable: true,
-    },
-    {
-      field: 'lastName',
-      headerName: 'Last name',
-      editable: true,
-    },
-    {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-      editable: true,
-    },
-    {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
-    },
-    {
-        field: 'email',
-        headerName: 'Email',
-        type: 'number',
-        editable: true,
-      },
-      {
-        field: 'phone',
-        headerName: 'Phone',
-        type: 'number',
-        editable: true,
-      },
-  ];
-  
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "firstName",
+    headerName: "First name",
+    editable: true,
+  },
+  {
+    field: "lastName",
+    headerName: "Last name",
+    editable: true,
+  },
+  {
+    field: "age",
+    headerName: "Age",
+    type: "number",
+    editable: true,
+  },
+  {
+    field: "fullName",
+    headerName: "Full name",
+    description: "This column has a value getter and is not sortable.",
+    sortable: false,
+    valueGetter: (value, row) => `${row.firstName || ""} ${row.lastName || ""}`,
+  },
+  {
+    field: "email",
+    headerName: "Email",
+    type: "number",
+    editable: true,
+  },
+  {
+    field: "phone",
+    headerName: "Phone",
+    type: "number",
+    editable: true,
+  },
+];
+
 const UserList: FC = () => {
   const [allUsers, setAllUsers] = useState([]);
+  const [totalUsers, setTotalUsers] = useState<number | undefined >(undefined); // rowCount is expecting a number or undefined .
+  const [page, setPage] = useState<number>(0); // Current page (0-based index)
+  const [pageSize, setPageSize] = useState<number>(10); // Number of rows per page [Basically its the limit]
   useEffect(() => {
-    fetchAllUsers().then((response) => {
-      setAllUsers(response.data.users)
+    fetchAllUsers(pageSize,page*pageSize).then((response) => {
+      setAllUsers(response.data.users);
+      setTotalUsers(response.data.total)
     });
-  },[]);
-
-  const rows = allUsers.map(({ firstName, lastName, age,email,phone,id }) => ({
-    firstName,
-    lastName,
-    age,
-    email,
-    phone,
-    id
-  }));
-  
+  }, [pageSize,page]);
 
 
-  
   return (
-    <Box sx={{ height: 400 }}>
-    <DataGrid
-      rows={rows}
-      columns={columns}
-      initialState={{
-        pagination: {
-          paginationModel: {
-            pageSize: 5,
-          },
-        },
-      }}
-      pageSizeOptions={[5]}
-      checkboxSelection
-      disableRowSelectionOnClick
-    />
-  </Box>
+    <Box className="user-list-table-container">
+      <Typography variant="h3">User Listing Table</Typography>
+      <DataGrid
+        rows={allUsers}
+        columns={columns}
+        rowCount={totalUsers}
+        paginationMode="server"
+        checkboxSelection
+        disableRowSelectionOnClick
+        onPaginationModelChange={(model) => {
+          // console.log("Pagination triggered")
+          // onPaginationModelChange will run in cases when pagination is triggered
+          if (model.pageSize !== pageSize) {
+            setPage(0); // Reset page to 0 if pageSize changes
+          }
+          setPage(model.page); 
+          setPageSize(model.pageSize); 
+        }}
+        pageSizeOptions={[10, 25, 50,100]} // Set allowed page sizes
+        paginationModel={{ page, pageSize }} // page, pageSize can only be used 
+        
+      />
+    </Box>
   );
 };
 
